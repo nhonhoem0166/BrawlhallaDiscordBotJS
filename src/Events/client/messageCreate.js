d0a8181f6366cac314574affc2887b5a4699b61f
Rot4tion <nhonhoem0123@gmail.com>
2022-07-31 12:31:11 +0700

    fix prefix 1

--- src/Events/client/messageCreate.js
+++ src/Events/client/messageCreate.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 module.exports = {
   name: "messageCreate",
   async execute(msg, client) {
     console.log(msg.content);
-    if(!msg.content.startsWith(client.prefix) || msg.author.bot) return;
-    var cmdString = msg.content.substring(0,client.prefix.length);
+    if(!msg.content.startsWith(process.env.prefix) || msg.author.bot) return;
+    var cmdString = msg.content.substring(process.env.prefix.length, msg.content.length);
     const command = client.commands.get(cmdString);
     if (!command) return;
     command.execute(msg, client);
